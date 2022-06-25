f3d24b79ce6bdfe244430248f8848e0579741522
yashensha <m.yashensha@gmail.com>
2022-06-25 21:17:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./sass/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Basic react-sass template by yashensha.</p>
+    </div>
+  );
+}
+
+export default App;
