6952a72367d6fbeb8015a14bd75746e1c522c43c
shakoor123 <shakoorchachu2001@gmail.com>
2022-06-27 23:29:17 +0530

    bug fix

--- Server/index.js
+++ Server/index.js
@@ -5,13 +5,13 @@ const passport=require('passport')
 const User=require('./models/user')
 const messageRoute=require('./routes/message')
 var cors = require('cors')
-
+require('dotenv').config()
 app.use(express.json());
 app.use(cors());
 app.use('/message',messageRoute)
 
 //database connection
-mongoose.connect('mongodb+srv://shakoor:shakoor@cluster0.40mzq.mongodb.net/?retryWrites=true&w=majority',()=>{
+mongoose.connect(process.env.URL,()=>{
     console.log("Mongoose connected");
 });
 //server running
@@ -26,8 +26,8 @@ const session = require('express-session');
 app.use(session({ secret: 'melody hensley is my spirit animal' }));
 let pro;
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
-var GOOGLE_CLIENT_ID= "895657809795-1r3fhcmf6icsgl2kb3o3qghiu6emblhl.apps.googleusercontent.com"
-var GOOGLE_CLIENT_SECRET= "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"
+var GOOGLE_CLIENT_ID= process.env.GOOGLE_CLIENT_ID
+var GOOGLE_CLIENT_SECRET= process.env.GOOGLE_CLIENT_SECRET
 passport.use(
     new GoogleStrategy(
       {
@@ -69,19 +69,22 @@ passport.deserializeUser((user, done) => {
 app.get("/login/success",async (req, res) => {
     let profile=await pro;
     if (    profile) {
+      const newUser=await new User({
+        googleId:profile.id,
+        name:profile.displayName,
+        image:profile.photos[0].value,
+    })
         try {
-
-            const newUser=await new User({
-                googleId:profile.id,
-                name:profile.displayName,
-                image:profile.photos[0].value,
-            })
-            
+          const cuser=await User.findOne({ googleId:profile.id })
+          if(cuser){
+            res.status(200).json(cuser)
+          }else{
             const user=await newUser.save();
             res.status(200).json(user._doc)
+          }
             } catch (err) {
-              const cuser=await User.findOne({ googleId:err.keyValue.googleId })
-                res.status(200).json(cuser)
+             // const cuser=await User.findOne({ googleId:err.keyValue.googleId })
+                res.status(500).json(err)
             }
     }
   });
